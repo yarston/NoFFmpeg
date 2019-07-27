60ea918d38e010c2921bf7b76b4188435e725f8e
slava <yarston@gmail.com>
2019-07-27 12:14:00 +0300

    optimize yuv2rgb

--- app/src/main/cpp/yuv2rgb.c
+++ app/src/main/cpp/yuv2rgb.c
@@ -46,42 +46,72 @@ Java_com_slava_noffmpeg_mediaworkers_VideoProcessor_cvtYUV_1420_1888_1to_1RGBA(J
     uint8_t *u = (*env)->GetDirectBufferAddress(env, buff_u);
     uint8_t *v = (*env)->GetDirectBufferAddress(env, buff_v);
 
-    int width = info.width, height = info.height, width1 = width - 1;
+    int width = info.width, height = info.height, width1 = width - 1, height1 = height - 1;
 
     #pragma omp parallel for
-    for (uint32_t i = 0; i < height; i++) {
+    for (uint32_t i = 0; i < height1; i += 2) {
 
-        uint32_t idx_y = i * width, idx_uv = (i / 2) * (width / 2);
-        uint32_t *row_out = pixels + idx_y;
-        uint8_t *row_y = y + idx_y;
+        uint32_t idx_y1 = i * width, idx_y2 = idx_y1 + width, idx_uv = (i / 2) * (width / 2);
+        uint32_t *row_out1 = pixels + idx_y1, *row_out2 = pixels + idx_y2;
+        uint8_t *row_y1 = y + idx_y1, *row_y2 = y + idx_y2;
         uint8_t *row_u = u + idx_uv;
         uint8_t *row_v = v + idx_uv;
 
         for (uint32_t j = 0; j < width1; j += 2) {
-            int Y = *row_y++;
+            float Y = *row_y1++;
             int U = *row_u++ - 128;
             int V = *row_v++ - 128;
 
-            int32_t R = (int32_t) (Y + 1.140 * V);
-            int32_t G = (int32_t) (Y - 0.395 * U - 0.581 * V);
-            int32_t B = (int32_t) (Y + 2.032 * U);
+            float Ug = -0.395f * U;
+            float Ub = 2.032f * U;
+            float Vr = 1.140f * V;
+            float Vg = -0.581f * V;
+
+            int32_t R = (int32_t) (Y + Vr);
+            int32_t G = (int32_t) (Y + Ug + Vg);
+            int32_t B = (int32_t) (Y + Ub);
 
             CLAMP(R)
             CLAMP(G)
             CLAMP(B)
 
-            *row_out++ = R | (G << 8) | (B << 16) | 0xFF000000;
+            *row_out1++ = R | (G << 8) | (B << 16) | 0xFF000000;
 
-            Y = *row_y++;
+            Y = *row_y1++;
 
-            R = (int32_t) (Y + 1.140 * V);
-            G = (int32_t) (Y - 0.395 * U - 0.581 * V);
-            B = (int32_t) (Y + 2.032 * U);
+            R = (int32_t) (Y + Vr);
+            G = (int32_t) (Y + Ug + Vg);
+            B = (int32_t) (Y + Ub);
 
             CLAMP(R)
             CLAMP(G)
             CLAMP(B)
-            *row_out++ = R | (G << 8) | (B << 16) | 0xFF000000;
+
+            *row_out1++ = R | (G << 8) | (B << 16) | 0xFF000000;
+
+            Y = *row_y2++;
+
+            R = (int32_t) (Y + Vr);
+            G = (int32_t) (Y + Ug + Vg);
+            B = (int32_t) (Y + Ub);
+
+            CLAMP(R)
+            CLAMP(G)
+            CLAMP(B)
+
+            *row_out2++ = R | (G << 8) | (B << 16) | 0xFF000000;
+
+            Y = *row_y2++;
+
+            R = (int32_t) (Y + Vr);
+            G = (int32_t) (Y + Ug + Vg);
+            B = (int32_t) (Y + Ub);
+
+            CLAMP(R)
+            CLAMP(G)
+            CLAMP(B)
+
+            *row_out2++ = R | (G << 8) | (B << 16) | 0xFF000000;
         }
     }
 #endif
