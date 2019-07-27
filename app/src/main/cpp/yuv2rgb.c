3297b2633c6bc24d0cbc575d2ea78c92718dd2f8
slava <yarston@gmail.com>
2019-07-27 07:42:56 +0300

    naive c implementation of converter yuv -> rgba , 7s / 100 frames

--- app/src/main/cpp/yuv2rgb.c
+++ app/src/main/cpp/yuv2rgb.c
@@ -1,14 +1,15 @@
 #include <jni.h>
-#include <time.h>
 #include <android/log.h>
 #include <android/bitmap.h>
-
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define  LOG_TAG    "libplasma"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+#define CLAMP(x) if (x > 255) {x = 255;} else if (x < 0) { x = 0;}
 
 
 JNIEXPORT void JNICALL
@@ -18,8 +19,12 @@ Java_com_slava_noffmpeg_mediaworkers_VideoProcessor_cvtYUV_1420_1888_1to_1RGBA(J
                                                                                jobject buff_y,
                                                                                jobject buff_u,
                                                                                jobject buff_v) {
+    int8_t *y = (*env)->GetDirectBufferAddress(env, buff_y);
+    int8_t *u = (*env)->GetDirectBufferAddress(env, buff_u);
+    int8_t *v = (*env)->GetDirectBufferAddress(env, buff_v);
+
     AndroidBitmapInfo info;
-    void *pixels;
+    uint32_t *pixels;
     int ret;
 
     if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
@@ -32,10 +37,34 @@ Java_com_slava_noffmpeg_mediaworkers_VideoProcessor_cvtYUV_1420_1888_1to_1RGBA(J
         return;
     }
 
-    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+    if ((ret = AndroidBitmap_lockPixels(env, bitmap, (void**) &pixels)) < 0) {
         LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
     }
 
-    for (uint32_t *p = pixels, *e = p + info.width * info.height; p < e; p++) *p = 0xFF00FF00;
+    //for (uint32_t *p = pixels, *e = p + info.width * info.height; p < e; p++) *p = 0xFF00FF00;
+
+    int width = info.width, height = info.height;
+
+    for (uint32_t i = 0; i < height; i++) {
+        for (uint32_t j = 0; j < width; j++) {
+            uint32_t pos1 = i * width + j;
+            uint32_t pos2 = (i / 2) * (width / 2) + j / 2;
+            int Y = y[pos1] & 0xFF;
+            int U = u[pos2] & 0xFF;
+            int V = v[pos2] & 0xFF;
+            U = U - 128;
+            V = V - 128;
+            int32_t R, G, B;
+            R = (int32_t) (Y + 1.140 * V);
+            G = (int32_t) (Y - 0.395 * U - 0.581 * V);
+            B = (int32_t) (Y + 2.032 * U);
+
+            CLAMP(R)
+            CLAMP(G)
+            CLAMP(B)
+            pixels[i * width + j] = R | (G << 8) | (B << 16) | 0xFF000000;
+        }
+    }
+
     AndroidBitmap_unlockPixels(env, bitmap);
 }
